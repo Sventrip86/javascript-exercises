150f335d36cd2b40b47b61e8a97ac7e6de11546d
Fabs <fab.sebis@google.com>
2022-12-21 20:22:48 +0100

    one and two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+
+
+
+const repeatString = (word, number) => {
+
+    let words = "";
+
+    for(let i=0; i < number; i++){
+        words = words + word 
+    }
+
+    return words
 
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
