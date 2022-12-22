9a7510b1744e38a76b441b112aec01b6ba3ee2b7
Fabs <fab.sebis@google.com>
2022-12-22 18:07:35 +0100

    exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = (word) => {
+    
+    return word.split('').reverse().join('')
 
 };
-
+reverseString('ciao come, ')
 // Do not edit below this line
 module.exports = reverseString;
